855e6baa632389c4cc920ddb6bc6c6acb2074809
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2021-07-20 22:28:41 +0530

    added file enc dec

---
+++ fileEncDec.py
@@ -0,0 +1,46 @@
+import base64
+import os
+import warnings
+from . import processKiller
+
+
+def encodeFile(filepath :str):        
+    try:
+        open(filepath+".ncod",'x')
+    except FileExistsError:
+        pass
+    
+    with open(filepath,'rb') as file , open(filepath+".ncod",'wb') as writer:
+        try:
+            writer.write(base64.b64encode(file.read()))
+            writer.close()
+            file.close()
+            print(file.name)
+            os.remove(file.name)
+        except PermissionError as e:
+            processKiller.killPID()
+            warnings.warn("occured {e}")
+        except Exception:
+            os.remove(writer.name)
+
+
+
+def decodeFile(filepath:str):
+    try:
+        open(filepath,'x')
+    except FileExistsError:
+        pass
+    with open(filepath+".ncod",'rb') as file , open(filepath,'wb') as writer:
+        try:
+            writer.write(base64.b64decode(file.read()))
+            writer.close()
+            file.close()
+            print(file.name)
+            os.remove(file.name)
+        except PermissionError as e:
+            processKiller.killPID()
+            warnings.warn("occured {e}")
+        except Exception:
+            writer.close()
+            os.remove(writer.name)
+    
---
+++ processKiller.py
@@ -0,0 +1,2 @@
+def killPID():
+    pass
